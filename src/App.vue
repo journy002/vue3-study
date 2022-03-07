39b60d280d739619a6d67e0f84f9227288f4c2a2
journy002 <journy002@naver.com>
2022-03-07 23:04:41 +0900

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div>
-    <h4>xx 원룸</h4>
+    <h3>원룸샵</h3>
+    <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
   </div>
   <div>
-    <h4>xx 원룸</h4>
-    <p>xx 만원</p>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price3 }} 만원</p>
   </div>
 </template>
 
@@ -15,7 +20,10 @@ export default {
   name: "App",
   data() {
     return {
-      price1: "50",
+      price1: "150",
+      price2: "70",
+      price3: "80",
+      products: ["역삼동 원룸", "인덕원 원룸", "과천 원룸"],
     };
   },
   components: {},
